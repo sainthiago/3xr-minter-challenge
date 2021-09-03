1e9a4918ce408e198c08cf75442579bce5d96149
Rui Santiago <ruisantiagomr@gmail.com>
2021-09-03 15:24:50 +0100

    first commit, added a simple minter form and removed unused components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,13 @@
-import Head from 'next/head'
-
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import Products from '../components/Products'
-import MusicPlayer from '../components/MusicPlayer'
+import Form from "../components/Form";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>Mintbase Engineering</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Header />
-      {/* <Navbar /> */}
-      <Hero />
-      <Products storeId="wildeverse.mintbase1.near" />
-      <Products storeId="hellovirtualworld.mintbase1.near" />
-      <MusicPlayer />
-      <Footer />
+      <Form />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
