3584d62b34c299cadba6c6f82e7f2e7245b057b1
Rui Santiago <ruisantiagomr@gmail.com>
2021-09-09 00:12:31 +0100

    changed field order in details step

--- components/Form/Steps/GalleryDetails.tsx
+++ components/Form/Steps/GalleryDetails.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useFormContext } from "react-hook-form";
 import ColorPicker from "../Components/ColorPicker";
-import SearchBar from "../Components/SearchBar";
 
 const GalleryDetails = () => {
   const {
@@ -11,50 +10,6 @@ const GalleryDetails = () => {
 
   return (
     <div className="px-8 pt-6 pb-8 mb-4 w-3/6 m-auto ">
-      <div className="mb-8">
-        <label className="block text-xl text-white">DAO Address</label>
-        <p className="text-gray-400 mb-2 mb-4 text-xs">
-          Choose the DAO you want to propose the gallery creation
-        </p>
-        <input
-          {...register("daoAddress", { required: true })}
-          className="shadow appearance-none border-2 border-smoothPrimary rounded w-full py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline focus:text-gray-100 focus:border-accent bg-primary h-12"
-          disabled={true}
-          readOnly
-        />
-      </div>
-      <div className="mb-8">
-        <label className="block text-xl text-white">
-          Choose your gallery template
-        </label>
-        <p className="text-gray-400 mb-2 mb-4 text-xs">
-          Select the gallery template
-        </p>
-
-        <div className="flex">
-          <input className="hidden" type="text" />
-          <label className="flex flex-col rounded border-4 border-accent cursor-pointer w-2/6 mr-16">
-            <img
-              className="w-64 h-40 object-cover"
-              src={"/tree_gallery.jpeg"}
-              alt="Tree Gallery"
-            />
-          </label>
-          <input className="hidden" type="text" />
-          <label className="flex flex-col p-4 rounded border-2 border-gray-400 cursor-pointer w-2/6">
-            <span className="text-xs font-semibold uppercase text-gray-400 m-auto text-center">
-              SOON
-            </span>
-          </label>
-        </div>
-      </div>
-      <div className="mb-8">
-        <label className="block text-xl text-white">Color Picker</label>
-        <p className="text-gray-400 mb-2 mb-4 text-xs">
-          Choose the colors that suit your gallery the best
-        </p>
-        <ColorPicker />
-      </div>
       <div className="mb-8">
         <label className="block text-xl text-white">NFT Gallery Quantity</label>
         <p className="text-gray-400 mb-2 mb-4 text-xs">
@@ -71,6 +26,50 @@ const GalleryDetails = () => {
             Please select a quantity between 1 and 10.
           </p>
         )}
+      </div>
+      <div className="mb-8">
+        <label className="block text-xl text-white">
+          Choose your gallery template
+        </label>
+        <p className="text-gray-400 mb-2 mb-4 text-xs">
+          Select the gallery template
+        </p>
+
+        <div className="flex">
+          <input className="hidden" type="text" />
+          <label className="flex flex-col rounded border-4 border-accent cursor-pointer w-2/6 mr-16">
+            <img
+              className="w-full h-40 object-cover"
+              src={"/tree_gallery.jpeg"}
+              alt="Tree Gallery"
+            />
+          </label>
+          <input className="hidden" type="text" />
+          <label className="flex flex-col p-4 rounded border-2 border-gray-400 cursor-not-allowed w-2/6">
+            <span className="text-xs font-semibold uppercase text-gray-400 m-auto text-center">
+              SOON
+            </span>
+          </label>
+        </div>
+      </div>
+      <div className="mb-8">
+        <label className="block text-xl text-white">Color Picker</label>
+        <p className="text-gray-400 mb-2 mb-4 text-xs">
+          Choose the colors that suit your gallery the best
+        </p>
+        <ColorPicker />
+      </div>
+      <div className="mb-8">
+        <label className="block text-xl text-white">DAO Address</label>
+        <p className="text-gray-400 mb-2 mb-4 text-xs">
+          Choose the DAO you want to propose the gallery creation
+        </p>
+        <input
+          {...register("daoAddress", { required: true })}
+          className="shadow appearance-none border-2 border-smoothPrimary rounded w-full py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline focus:text-gray-100 focus:border-accent bg-primary h-12"
+          disabled={true}
+          readOnly
+        />
       </div>
     </div>
   );
